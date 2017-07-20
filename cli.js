0c91a79bfe56122cc90b6171b85ad43c7b9a9ea3
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2017-07-20 01:25:47 +0100

    good god, it works

--- cli.js
+++ cli.js
@@ -14,23 +14,11 @@ const cli = meow(`
    $ git-good wiki         # opens wiki page
    $ git-good settings     # opens settings page
    $ git-good contributors # opens contributors page
-   $ git-good new-pr       # opens new pull request page`, {});
+   $ git-good new-pr       # opens new pull request page`,
+{});
 
-const possibleCommands = [
-	'open', // https://github.com/user/repo
-	'issues', // https://github.com/user/repo/issues
-	'prs', // https://github.com/user/repo/pulls
-	'pr', // https://github.com/user/repo/pull/85 # find out which one
-	'releases', // https://github.com/user/repo/releases
-	'branches', // https://github.com/user/repo/branches
-	'wiki', // https://github.com/user/repo/wiki
-	'settings', // https://github.com/user/repo/settings
-	'contributors', // https://github.com/user/repo/graphs/contributors
-	'new-pr' // https://github.com/user/repo/compare?expand=1
-];
-
-if (cli.input.length > 0 && possibleCommands.indexOf(cli.input[0]) !== -1) {
-	lib.hello(cli.input[0]);
+if (cli.input.length > 0 && lib.isValidCommand(cli.input[0])) {
+	lib.init(cli.input[0]);
 } else {
 	cli.showHelp(2);
 }
--- index.js
+++ index.js
@@ -1,9 +1,61 @@
 const Chalk = require('chalk');
 const log = require('node-time-log').SimpleLog;
+const opn = require('opn');
+const repoName = require('git-repo-name');
+const gitUsername = require('git-username');
+const branch = require('git-branch');
+
+let mUsername = '';
+let mRepository = '';
+let mBranch = '';
+
+const possibleCommands = [
+	{command: 'open', url: 'https://github.com/{user}/{repo}'},
+	{command: 'issues', url: 'https://github.com/{user}/{repo}/issues'},
+	{command: 'prs', url: 'https://github.com/{user}/{repo}/pulls'},
+	{command: 'pr', url: 'https://github.com/{user}/{repo}/pull/85'}, //  # find out which one
+	{command: 'releases', url: 'https://github.com/{user}/{repo}/releases'},
+	{command: 'branches', url: 'https://github.com/{user}/{repo}/branches'},
+	{command: 'wiki', url: 'https://github.com/{user}/{repo}/wiki'},
+	{command: 'settings', url: 'https://github.com/{user}/{repo}/settings'},
+	{command: 'contributors', url: 'https://github.com/{user}/{repo}/graphs/contributors'},
+	{command: 'new-pr', url: 'https://github.com/{user}/{repo}/compare?expand=1'}
+];
+
+function getProperURL(url, user, repo) {
+	return url.replace('{user}', user).replace('{repo}', repo);
+}
+
+function getCommandFromArray(command) {
+	const found = possibleCommands
+		.filter(item => {
+			return item.command === command;
+		});
+	return found.length === 0 ? {} : found[0];
+}
+
+function open(url) {
+	opn(url).then(() => {
+		log(`opened: ${Chalk.green(url)}!`);
+	});
+}
 
 // Main code //
 module.exports = {
-	hello: command => {
-		log(`run: ${Chalk.green(command)}!`);
+	isValidCommand: command => {
+		const result = getCommandFromArray(command);
+		return Object.keys(result).length !== 0;
+	},
+	init: command => {
+		mBranch = branch.sync();
+		mUsername = gitUsername();
+		mRepository = repoName.sync();
+
+		const result = getCommandFromArray(command);
+
+		log(`${mUsername}/${mRepository} - ${mBranch}`);
+		const properUrl = getProperURL(result.url, mUsername, mRepository);
+
+		open(properUrl);
 	}
 };
